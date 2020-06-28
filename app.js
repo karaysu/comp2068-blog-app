46640d87e2d339f7c6dd0d19352931c515ab15ab
karaysu <deepraman90@gmail.com>
2020-06-28 14:33:04 -0400

    Added new, create, show

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 const path = require('path');
 
@@ -7,10 +8,30 @@ const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use('/css', express.static('assets/css'));
+app.use('/javascript', express.static('assets/javascript'));
+app.use('/images', express.static('assets/images'));
+
+//Mongo Access
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI, {
+		auth: {
+			user: process.env.DB_USER,
+			password: process.env.DB_PASS,
+		},
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+    .catch((err) => console.error(`Error: ${err}`));
+    
+//Implement Body Parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Our routes
 const routes = require('./routes.js');
 app.use('/', routes);
 
 //Start out server
-app.listen(process.env.PORT || 3000);
- 
+app.listen(process.env.PORT || 3000, (port) => console.log(`Listening on port ${port}`));
---
+++ controllers/BlogsController.js
@@ -0,0 +1,42 @@
+const viewPath = 'blogs';
+const Blog = require('../models/blog');
+const blog = require('../models/blog');
+
+exports.index = (req, res) => {
+	res.send('Index');
+};
+
+exports.show = async (req, res) => {
+	const blog = await Blog.findById(req.params.id);
+	res.render(`${viewPath}/show`, {
+		pageTitle: blog.title,
+		blog: blog
+	});
+};
+
+exports.new = (req, res) => {
+	res.render(`${viewPath}/new`, {
+		pageTitle: 'New Blog',
+	});
+};
+
+exports.create = async (req, res) => {
+	try {
+		const blog = await Blog.create(req.body);
+		res.redirect(`/blogs/${blog.id}`);
+	} catch (err) {
+		res.send(err);
+	}
+};
+
+exports.edit = (req, res) => {
+	res.send('Hey');
+};
+
+exports.update = (req, res) => {
+	res.send('whoop');
+};
+
+exports.delete = (req, res) => {
+	res.send('Goodbye');
+};
