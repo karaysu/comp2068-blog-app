3792a05a322064c94ac370944cb7a284f2bea6a7
karaysu <deepraman90@gmail.com>
2020-08-02 14:39:17 -0400

    added register

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const {new: _new, create} = require('../controllers/UsersController')
 
 module.exports  = router => {
-    router.use('/', () => console.log("users route"));
     router.get('/register', _new);
     router.post('/users', create);
 };
